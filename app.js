04883bebfbe2922b125d1739cec8794bc2c2bb73
Don Nnguyen <donng24@gmail.com>
2023-01-09 21:28:02 -0600

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => res.send("Hello World!"));
+const morgan = require("morgan");
+const postBank = require("./postBank")
 
 app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+
+const posts = postBank.list();
+
+const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title> Wizard News</title>
+  </head>
+  <body>
+      <ul>
+        ${posts.map(post => `<li>${posts}</li>`)}
+      </ul>
+    </body>
+    </html>`
+
+res.send(html);
+});
+
+
+
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
+
