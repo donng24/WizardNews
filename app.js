f4b75cde57cadd13801fb7317514c5e5a29151b0
Don Nnguyen <donng24@gmail.com>
2023-03-20 14:46:25 -0500

    wizard

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const DATABASE_URL="postgres://wizardnews_ifup_user:uG9LhvvY00CrF7VMMLhCdcRkT07zVzra@dpg-cgcb81t269v4icvjk7mg-a.ohio-postgres.render.com/wizardnews_ifup?ssl=true"
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
@@ -5,29 +6,48 @@ const postBank = require("./postBank")
 
 app.use(morgan('dev'));
 
-app.get("/", (req, res) => {
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  
+  if (!post.id) {
+ res.status(404)
 
 const posts = postBank.list();
 
 const html = `<!DOCTYPE html>
-  <html>
-  <head>
-    <title> Wizard News</title>
-  </head>
-  <body>
-      <ul>
-        ${posts.map(post => `<li>${posts}</li>`)}
-      </ul>
-    </body>
-    </html>`
+<html>
+<head>
+  <title>Wizard News</title>
+  <link rel="stylesheet" href="/style.css" />
+</head>
+<body>
+  <div class="news-list">
+    <header><img src="/logo.png"/>Wizard News</header>
+    ${posts.map(post => `
+      <div class='news-item'>
+        <p>
+          <span class="news-position">${post.id}. ▲</span>
+          ${post.title}
+          <small>(by ${post.name})</small>
+        </p>
+        <small class="news-info">
+          ${post.upvotes} upvotes | ${post.date}
+        </small>
+      </div>`
+    ).join('')}
+  </div>
+</body>
+</html>`
 
 res.send(html);
-});
+}
+else {
+
+}});
 
 
-
-
-const PORT = 1337;
+const { PORT = 1337 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
